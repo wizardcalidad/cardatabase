8b63c0ad0f5bd89fe32e4c4b27a91c5a225cfe4b
Qoyum Yusuf <yusufqoyum01@gmail.com>
2020-10-18 22:07:14 +0100

    Class of Owner and OwnerRepository added

--- src/main/java/com/first/cardatabase/domain/CarRepository.java
+++ src/main/java/com/first/cardatabase/domain/CarRepository.java
@@ -1,12 +1,32 @@
 package com.first.cardatabase.domain;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
 
 public interface CarRepository extends CrudRepository<Car, Long> {
-//Fetch cars by attributes
+//Fetch cars by an attribute
     List<Car> findByBrand(String brand);
     List<Car> findByColor(String color);
     List<Car> findByYear(int year);
+
+    // Fetch by multiple attributes
+    List<Car> findByBrandAndModel(String brand, String model);
+    List<Car> findByBrandOrColor(String brand, String color);
+
+    // Fetch cars by brand and sort by year
+    List<Car> findByBrandOrderByYearAsc(String brand);
+
+    // Fetch cars by brand using SQL
+    //@Query("select c from Car c where c.brand = ?1")
+    //List<Car> findByBrand(String brand);
+
+    // Fetch cars by brand using SQL
+    @Query("select c from Car c where c.brand like %?1")
+    List<Car> findByBrandEndsWith(String brand);
+
+
+
+
 }
---
+++ src/main/java/com/first/cardatabase/domain/OwnerRepository.java
@@ -0,0 +1,4 @@
+package com.first.cardatabase.domain;
+
+public interface OwnerRepository {
+}
