51b57bf842a0ff5a53effe73c8ae66cc73262593
Qoyum Yusuf <yusufqoyum01@gmail.com>
2020-10-18 21:15:07 +0100

    Added commandLineRunner

--- src/main/java/com/first/cardatabase/CardatabaseApplication.java
+++ src/main/java/com/first/cardatabase/CardatabaseApplication.java
@@ -2,8 +2,10 @@ package com.first.cardatabase;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class CardatabaseApplication {
@@ -12,7 +14,13 @@ public class CardatabaseApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CardatabaseApplication.class, args);
-		logger.info("Hello Spring Boot");
+	}
+
+	@Bean
+	CommandLineRunner runner(){
+		return args -> {
+			// Place your code here
+		};
 	}
 
 }
